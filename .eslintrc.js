3865b460cf6a1d3567e8168f52f87b5c1f50f2fe
Shawn Ranck <30805656+smranck@users.noreply.github.com>
2018-12-07 21:44:50 -0800

    Continue building framework for app production

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: './node_modules/eslint-config-airbnb/index.js',
+  rules: {
+    'prefer-const': 0,
+    'no-console': 0,
+    'react/prefer-stateless-function': 0,
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react', 'es2015', 'stage-0'],
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000',
+      },
+    ],
+  },
+};
